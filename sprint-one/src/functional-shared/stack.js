f18a6aaa86213a4d6ae1c252f95d23a1c871f2c9
brandontiqui <btiqui@gmail.com>
2016-07-27 16:05:06 -0700

    Add size reporting for the stack in functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+
+  instance.size = stackMethods.size;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  'size': function() {
+    return Object.keys(this.storage).length;
+  },
+
+  'push': function(val) {
+  }
+};
+
+
+
 
 
