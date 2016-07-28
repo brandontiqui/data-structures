ac7a61af8030e3ee531dd7d2059b3c99ebdb3b64
brandontiqui <btiqui@gmail.com>
2016-07-28 09:43:39 -0700

    Pseudoclassical Queue: Implemented dequeue.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -7,7 +7,16 @@ Queue.prototype.enqueue = function (val) {
 };
 
 Queue.prototype.dequeue = function () {
+  for (key in this.storage) {
+    if (key === '0') {
+      var val = this.storage[key];
+    } else {
+      this.storage[key - 1] = this.storage[key];
+    }
+  }
 
+  delete this.storage[this.size() - 1];
+  return val;
 };
 
 Queue.prototype.size = function () {
