43afc4efc6d169fb7b3ece38ec495c9008d39d9c
brandontiqui <btiqui@gmail.com>
2016-07-28 09:16:18 -0700

    Prototypal Queue: Implemented size reporting.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  'enqueue': function () {
+
+  },
+  'dequeue': function () {
+
+  },
+  'size': function () {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
