8f8f654d7457b49bfc3479d6ec15f50e3214012a
brandontiqui <btiqui@gmail.com>
2016-07-28 09:32:38 -0700

    Pseudoclassical pattern: implement stack size reporting

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Stack.prototype.push = function (val) {
+
+};
+
+Stack.prototype.pop = function () {
+
+};
+
+Stack.prototype.size = function () {
+  return Object.keys(this.storage).length;
+};
+
 
 
