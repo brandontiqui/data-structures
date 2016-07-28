99bd7f1b32caf5f92eab1596037e4aad86324124
brandontiqui <btiqui@gmail.com>
2016-07-28 09:21:55 -0700

    Prototypal Queue: Implemented dequeue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -10,7 +10,15 @@ var queueMethods = {
     this.storage[this.size()] = val;
   },
   'dequeue': function () {
-
+    for (key in this.storage) {
+      if (key === '0') {
+        var val = this.storage[key];
+      } else {
+        this.storage[key - 1] = this.storage[key];
+      }
+    }
+    delete this.storage[this.size() - 1];
+    return val;
   },
   'size': function () {
     return Object.keys(this.storage).length;
