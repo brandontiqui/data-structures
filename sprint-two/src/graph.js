a17ed287e7c6a132a959cd364bf78a54acb7882c
brandontiqui <btiqui@gmail.com>
2016-07-28 20:13:59 -0700

    Implement addEdge and hasEdge for the graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -17,7 +17,6 @@ Graph.prototype.addNode = function(node) {
 Graph.prototype.contains = function(node) {
   var isFound = false;
   this.storage.forEach(function(vertex) {
-    console.log(typeof vertex.data + ':' + typeof node);
     if (vertex.data === node) {
       isFound = true;
     }
@@ -27,14 +26,37 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  this.storage.forEach(function (vertex, index, collection) {
+    if (vertex.data === node) {
+      collection = collection.splice(index, 1);
+    }
+  });
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var isFound = false;
+  this.storage.forEach(function (vertex) {
+    if (vertex.edges.indexOf(fromNode) !== -1 && vertex.data === toNode) {
+      isFound = true;
+    }
+  });
+  return isFound;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage.forEach(function (vertex) {
+    if (vertex.data === fromNode) {
+      if (vertex.edges.indexOf(toNode) === -1) {
+        vertex.edges.push(toNode);
+      }
+    } else if (vertex.data === toNode) {
+      if (vertex.edges.indexOf(fromNode) === -1) {
+        vertex.edges.push(fromNode);
+      }
+    }
+  });
 };
 
 // Remove an edge between any two specified (by value) nodes.
